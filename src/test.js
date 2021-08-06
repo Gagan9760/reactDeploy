1e9dfacd29c92f355d69d8f8249a89cb34564566
Gagandeep Singh <g2mgs4@gmail.com>
2021-08-06 10:21:11 +0530

    full

---
+++ src/test.js
@@ -0,0 +1,13 @@
+meter1 = 3
+meter2 = 5
+cm1 = 30
+cm2 = 100
+totalM = meter1 + meter2
+totalCm = cm1 + cm2
+if(totalCm  >   100)
+    {
+        totalM = totalM + 1
+        totalCm = totalCm - 100
+    }
+console.log(`The total measurement is
+            ${totalM} meters ${totalCm} cm`);      
